33fa197987a3e4ee0d35eb3e6859e0a728f4354a
Michael Babker <mbabker@flbab.com>
2012-05-05 02:52:56 +0200

    First commit of BabDev Library
    This commit adds the structure for the library with a version class and basic test suite bootstrap.

---
+++ libraries/babdev/version/version.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * @package     BabDev.Library
+ * @subpackage  Version
+ *
+ * @copyright   Copyright (C) 2012 Michael Babker. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE
+ */
+
+defined('JPATH_PLATFORM') or die;
+
+/**
+ * Version information class for the BabDev Libraries.
+ *
+ * @package  BabDev.Libraries
+ * @since    1.0
+ */
+final class BDVersion
+{
+	// Product name.
+	const PRODUCT = 'BabDev Library';
+
+	// Major version.
+	const MAJOR = '1';
+
+	// Minor version.
+	const MINOR = '0';
+
+	// Maintenance version.
+	const MAINTENANCE = '0.alpha';
+
+	// Development STATUS.
+	const STATUS = 'Alpha';
+
+	// Release date.
+	const RELEASE_DATE = '05-May-2012';
+
+	// Release time.
+	const RELEASE_TIME = '00:00';
+
+	// Release timezone.
+	const RELEASE_TIME_ZONE = 'GMT';
+
+	// Copyright Notice.
+	const COPYRIGHT = 'Copyright (C) 2012 Michael Babker. All rights reserved.';
+
+	/**
+	 * Compares a PHP standard version number against the current library version.
+	 *
+	 * @param   string  $minimum  The minimum version of the library which is compatible.
+	 *
+	 * @return  boolean  True if the version is compatible.
+	 *
+	 * @see     http://www.php.net/version_compare
+	 * @since   1.0
+	 */
+	public static function isCompatible($minimum)
+	{
+		return (version_compare(self::getShortVersion(), $minimum, 'eq') == 1);
+	}
+
+	/**
+	 * Gets a PHP standard version string for the current library.
+	 *
+	 * @return  string  Version string.
+	 *
+	 * @since   1.0
+	 * @codeCoverageIgnore
+	 */
+	public static function getShortVersion()
+	{
+		return self::MAJOR . '.' . self::MINOR . '.' . self::MAINTENANCE;
+	}
+
+	/**
+	 * Gets a version string for the current library with all release information.
+	 *
+	 * @return  string  Complete version string.
+	 *
+	 * @since   1.0
+	 * @codeCoverageIgnore
+	 */
+	public static function getLongVersion()
+	{
+		return self::PRODUCT . ' ' . self::MAJOR . '.' . self::MINOR . '.' . self::MAINTENANCE . ' ' . self::STATUS . ' '
+			. self::RELEASE_DATE . ' ' . self::RELEASE_TIME . ' ' . self::RELEASE_TIME_ZONE;
+	}
+}
