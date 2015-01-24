6c4de1a79a0cbf8fea0692ea5e9c62180500b229
Michael Babker <mbabker@flbab.com>
2015-01-24 18:55:02 -0500

    2015

--- src/Projects.php
+++ src/Projects.php
@@ -2,7 +2,7 @@
 /**
  * BabDev Transifex Package
  *
- * @copyright  Copyright (C) 2012-2014 Michael Babker. All rights reserved.
+ * @copyright  Copyright (C) 2012-2015 Michael Babker. All rights reserved.
  * @license    http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License Version 2 or Later
  */
 
--- tests/ProjectsTest.php
+++ tests/ProjectsTest.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @copyright  Copyright (C) 2012-2014 Michael Babker. All rights reserved.
+ * @copyright  Copyright (C) 2012-2015 Michael Babker. All rights reserved.
  * @license    http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License Version 2 or Later
  */
 
